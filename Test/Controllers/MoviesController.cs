2703ab4fc8e82bc6aa1df13d4b525654796e0234
erlangmaestro <alexandermarkov@live.co.uk>
2017-02-22 22:02:01 +0000

    Version1
    Added a few custom models view and controllers to understand how the
    pattern works. Changed some of the html code and started customizing the
    bootstrap framework.

---
+++ Test/Controllers/MoviesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Test.Models;
+
+namespace Test.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            List<Movie> movies = new List<Movie> { new Movie { Name = "Predator", Id = 1}, new Movie { Name = "Alien", Id = 2 },
+                new Movie { Name = "Finding Nemo", Id = 3 }, new Movie { Name = "Blackjack", Id = 4 } };
+            ViewBag.MyList = movies;
+            return View();
+        }
+    }
+}
